e6006bcd5faa267995e3283f3035e14adbf5f5c1
s-justina <jus.sobcz@gmail.com>
2020-07-21 16:01:09 +0200

    handle input and btn

--- src/containers/AddTodo.js
+++ src/containers/AddTodo.js
@@ -1,10 +1,17 @@
 import React from 'react';
 
-const AddTodo = ()=>{
-    return(
+const AddTodo = () => {
+    let input;
+    const handleSubmit = (e) => {
+        const value = input.value.trim();
+        console.log(value);
+    };
+    return (
         <div>
-            <input type="text"/>
-            <button type='submit'>Add new task</button>
+            <input type="text" ref={node => {
+                input = node
+            }}/>
+            <button type='submit' onClick={handleSubmit}>Add new task</button>
         </div>
     )
 };
