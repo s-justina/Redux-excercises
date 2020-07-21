b2b917e95c46e9066aef107660b3292c7f009590
s-justina <jus.sobcz@gmail.com>
2020-07-21 15:48:13 +0200

    create input and btn for user

---
+++ src/containers/AddTodo.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const AddTodo = ()=>{
+    return(
+        <div>
+            <input type="text"/>
+            <button type='submit'>Add new task</button>
+        </div>
+    )
+};
+
+export default AddTodo;
